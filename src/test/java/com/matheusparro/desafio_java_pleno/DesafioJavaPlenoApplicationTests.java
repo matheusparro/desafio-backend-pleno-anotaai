01d2547ac7678f5d9727f9dc1e24dea639f380d9
Matheus Parro <mathparro@gmail.com>
2024-07-01 21:47:44 -0300

    feat: init test

---
+++ src/test/java/com/matheusparro/desafio_java_pleno/DesafioJavaPlenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusparro.desafio_java_pleno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaPlenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
