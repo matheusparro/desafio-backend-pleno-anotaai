e72e3d8bf0a1b8d6e06c73264bd0f073376f53ff
Matheus Parro <mathparro@gmail.com>
2024-07-01 21:45:34 -0300

    feat: add domains dtos

---
+++ src/main/java/com/matheusparro/desafio_java_pleno/domain/Category/CategoryDTO.java
@@ -0,0 +1,4 @@
+package com.matheusparro.desafio_java_pleno.domain.Category;
+
+public record CategoryDTO(String title, String description, String ownerId) {
+}
---
+++ src/main/java/com/matheusparro/desafio_java_pleno/domain/Product/ProductDTO.java
@@ -0,0 +1,4 @@
+package com.matheusparro.desafio_java_pleno.domain.Product;
+
+public record ProductDTO(String title, String description, Integer price, String ownerId, String categoryId) {
+}
